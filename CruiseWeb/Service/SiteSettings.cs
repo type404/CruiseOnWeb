ae8a8675e88fb45812a6397a85fb608118207df5
Tisa M <coderbychoice01@gmail.com>
2019-11-24 19:25:26 +1100

    Removed API Key

--- CruiseWeb/Service/SiteSettings.cs
+++ CruiseWeb/Service/SiteSettings.cs
@@ -1,5 +1,5 @@
 ﻿public static class SiteSettings
 {
-    public const string GoogleRecaptchaSecretKey = "6LcWNb8UAAAAAKmvCQJEuZ8Lh7WHi404LiNVpM-j";
-    public const string GoogleRecaptchaSiteKey = "6LcWNb8UAAAAALQ5RbXMSJ7Zu1Bb89qOprbSElRA";
+    public const string GoogleRecaptchaSecretKey = "XXXXXX";
+    public const string GoogleRecaptchaSiteKey = "XXXXX";
 }
